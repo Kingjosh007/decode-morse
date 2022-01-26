09755f381a240dce0c0042c56cb2a5bbb7f275ae
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-26 15:44:02 +0100

    Fix Linters errors

--- main.rb
+++ main.rb
@@ -1,60 +1,27 @@
-def decode_morse_char(s)
-  morse_hash = { 
-    "A" => ".-", 
-    "B" => "-...",
-    "C" => "-.-.",
-    "D" => "-..",
-    "E" => ".",
-    "F" => "..-.",
-    "G" => "--.",
-    "H" => "....",
-    "I" => "..",
-    "J" => ".---",
-    "K" => "-.-",
-    "L" => ".-..",
-    "M" => "--",
-    "N" => "-.",
-    "O" => "---",
-    "P" => ".--.",
-    "Q" => "--.-",
-    "R" => ".-.",
-    "S" => "...",
-    "T" => "-",
-    "U" => "..-",
-    "V" => "..-",
-    "W" => ".--",
-    "X" => "-..-",
-    "Y" => "-.--",
-    "Z" => "--..",
-    "0" => "-----",
-    "1" => ".----",
-    "2" => "..---",
-    "3" => "...--",
-    "4" => "....-",
-    "5" => ".....",
-    "6" => "-....",
-    "7" => "--...",
-    "8" => "---..",
-    "9" => "----."
+def decode_morse_char(str)
+  morse_hash = {
+    'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.',
+    'G' => '--.', 'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
+    'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.', 'Q' => '--.-', 'R' => '.-.',
+    'S' => '...', 'T' => '-', 'U' => '..-', 'V' => '..-', 'W' => '.--', 'X' => '-..-',
+    'Y' => '-.--', 'Z' => '--..', '0' => '-----', '1' => '.----', '2' => '..---', '3' => '...--',
+    '4' => '....-', '5' => '.....', '6' => '-....', '7' => '--...', '8' => '---..', '9' => '----.'
   }
-  value = morse_hash.select { |k,v| v == s }
-  return value.keys[0]
+  value = morse_hash.select { |_k, v| v == str }
+  value.keys[0]
 end
 
-
-def decode_morse_word(w)
-  w = w.split(" ")
-  w.map! { |x| decode_morse_char(x) }
-  return w.join("")
+def decode_morse_word(word)
+  word = word.split
+  word.map! { |x| decode_morse_char(x) }
+  word.join
 end
-decode_morse_word("-- -.--")
+decode_morse_word('-- -.--')
 
-def decode_morse_sentence(s)
-    s = s.split("   ")
-    s.map! { |x| decode_morse_word(x) }
-    return s.join(" ")
+def decode_morse_sentence(str)
+  str = str.split('   ')
+  str.map! { |x| decode_morse_word(x) }
+  str.join(' ')
 end
 
-decode_morse_sentence("-- -.--   -. .- -- .")
-
-
+decode_morse_sentence('-- -.--   -. .- -- .')
